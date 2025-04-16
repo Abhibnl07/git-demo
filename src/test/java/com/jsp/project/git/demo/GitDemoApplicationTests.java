9cacc6cf1a5dc6fc64c98894fd477dcfb263d7d5
akshitha <akshitaabhit@gmail.com>
2025-04-16 15:15:05 +0530

    first commit -root branch

---
+++ src/test/java/com/jsp/project/git/demo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.project.git.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
